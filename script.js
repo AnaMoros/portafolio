765ed21f491c31e7e7cc36b923976993b199b3ae
Ana <ana.marlein@gmail.com>
2021-06-11 23:45:52 +0100

    added overlay

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const smallNavBar = window.matchMedia("(min-width: 600px)");
 const openMenuBtn = document.getElementById("open-menu");
 const closeMenuBtn = document.getElementById("close-menu");
 const sideNav = document.getElementById("sidenav");
+const overlay = document.getElementById("overlay");
 smallNavBar.addEventListener("change", handleDeviceChange);
 
 function handleDeviceChange(e) {
   if (e.matches) {
     sideNav.classList.add("big-visibility");
     sideNav.classList.remove("ani-open-from-left");
+    overlay.classList.remove("overlay-open");
   } else {
     sideNav.classList.add("no-ani");
     sideNav.classList.remove("big-visibility");
@@ -17,10 +19,12 @@ function handleDeviceChange(e) {
 openMenuBtn.addEventListener("click", () => {
   sideNav.classList.remove("no-ani");
   sideNav.classList.add("ani-open-from-left");
+  overlay.classList.add("overlay-open");
 });
 
 closeMenuBtn.addEventListener("click", () => {
   sideNav.classList.remove("ani-open-from-left");
+  overlay.classList.remove("overlay-open");
 });
 
 //run initial size check
