73e7de9a094aa2c1263ef59239fd48eeab9b55f4
Ana <ana.marlein@gmail.com>
2021-06-11 18:44:40 +0100

    updated code to open and close the side navigation at different screen sizes

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-// open and close of side navbar for mobile view
-
+// open and close the side navigation when resizing the device
+const smallNavBar = window.matchMedia("(min-width: 600px)");
 const openMenuBtn = document.getElementById("open-menu");
 const closeMenuBtn = document.getElementById("close-menu");
 
+smallNavBar.addEventListener("change", handleDeviceChange);
+
+function handleDeviceChange(e) {
+  if (e.matches) {
+    console.log("bigger than 600px");
+    document.getElementById("sidenav").style.display = "unset";
+  } else {
+    console.log("smaller than 600px");
+    document.getElementById("sidenav").style.display = "none";
+  }
+}
 openMenuBtn.addEventListener("click", () => {
-  document.getElementById("sidenav").style.visibility = "visible";
+  document.getElementById("sidenav").style.display = "block";
 });
 
 closeMenuBtn.addEventListener("click", () => {
-  document.getElementById("sidenav").style.visibility = "hidden";
+  document.getElementById("sidenav").style.display = "none";
 });
+
+//run initial size check
+handleDeviceChange(smallNavBar);
