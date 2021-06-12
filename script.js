7f40070e6b51cb2b8590fde88696177ea268908e
Ana <ana.marlein@gmail.com>
2021-06-12 09:19:42 +0100

    added overlay when navbar is clicked, and function to close navbar by clicking the ovelay

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const openMenuBtn = document.getElementById("open-menu");
 const closeMenuBtn = document.getElementById("close-menu");
 const sideNav = document.getElementById("sidenav");
 const overlay = document.getElementById("overlay");
+const navLinks = document.getElementsByClassName("nav-links");
+console.log(navLinks);
 smallNavBar.addEventListener("change", handleDeviceChange);
 
 function handleDeviceChange(e) {
@@ -21,11 +23,15 @@ openMenuBtn.addEventListener("click", () => {
   sideNav.classList.add("ani-open-from-left");
   overlay.classList.add("overlay-open");
 });
-
-closeMenuBtn.addEventListener("click", () => {
+overlay.addEventListener("click", closeNav);
+closeMenuBtn.addEventListener("click", closeNav);
+for (link of navLinks) {
+  link.addEventListener("click", closeNav);
+}
+function closeNav() {
   sideNav.classList.remove("ani-open-from-left");
   overlay.classList.remove("overlay-open");
-});
+}
 
 //run initial size check
 handleDeviceChange(smallNavBar);
