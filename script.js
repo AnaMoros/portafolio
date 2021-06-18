daa1c644786823f47fb3fdbf10dc6ac4aa475432
Ana <ana.marlein@gmail.com>
2021-06-18 11:02:59 +0100

    changed classes/js when opening the navbar to make it more readable

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
+"use strict";
 // open and close the side navigation when resizing the device
-const smallNavBar = window.matchMedia("(min-width: 720px)");
+const navMinWidth = window.matchMedia("(min-width: 720px)");
 const openMenuBtn = document.getElementById("open-menu");
 const closeMenuBtn = document.getElementById("close-menu");
-const sideNav = document.getElementById("sidenav");
+const mainNav = document.getElementById("main-nav");
 const overlay = document.getElementById("overlay");
 const navLinks = document.getElementsByClassName("nav-links");
-smallNavBar.addEventListener("change", handleDeviceChange);
 
-function handleDeviceChange(e) {
-  if (e.matches) {
-    sideNav.classList.add("big-visibility");
-    sideNav.classList.remove("ani-open-from-left");
-    overlay.classList.remove("overlay-open");
-  } else {
-    sideNav.classList.add("no-ani");
-    sideNav.classList.remove("big-visibility");
-  }
-}
-openMenuBtn.addEventListener("click", () => {
-  sideNav.classList.remove("no-ani");
-  sideNav.classList.add("ani-open-from-left");
-  overlay.classList.add("overlay-open");
-});
-overlay.addEventListener("click", closeNav);
-closeMenuBtn.addEventListener("click", closeNav);
-for (link of navLinks) {
-  link.addEventListener("click", closeNav);
-}
-function closeNav() {
-  sideNav.classList.remove("ani-open-from-left");
-  overlay.classList.remove("overlay-open");
-}
+navMinWidth.addEventListener("change", handleDeviceChange);
 
 //run initial size check
-handleDeviceChange(smallNavBar);
+handleDeviceChange(navMinWidth);
+
+overlay.addEventListener("click", closeNav);
+closeMenuBtn.addEventListener("click", closeNav);
+for (let link of navLinks) {
+  link.addEventListener("click", closeNav);
+}
+
+openMenuBtn.addEventListener("click", () => {
+  mainNav.classList.remove("no-ani");
+  mainNav.classList.add("ani-open-from-left");
+  overlay.classList.add("overlay-open");
+});
+
+function handleDeviceChange(size) {
+  if (size.matches) {
+    mainNav.classList.add("visible", "topnav");
+    mainNav.classList.remove("ani-open-from-left", "sidenav");
+    overlay.classList.remove("overlay-open");
+  } else {
+    mainNav.classList.add("no-ani", "sidenav");
+    mainNav.classList.remove("visible", "topnav");
+  }
+}
+
+function closeNav() {
+  mainNav.classList.remove("ani-open-from-left");
+  overlay.classList.remove("overlay-open");
+}
 
 // Wrap every letter in a span
 const textWrapper = document.querySelector(".hero-animation");
